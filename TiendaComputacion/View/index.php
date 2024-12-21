ec45709771130c92f0b98925b58ddf845c0314b9
Eddier511 <124939498+Eddier511@users.noreply.github.com>
2024-12-21 08:14:25 -0600

    Proyecto Listo - Titles Update

--- TiendaComputacion/View/index.php
+++ TiendaComputacion/View/index.php
@@ -10,7 +10,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2-beta3/css/all.min.css">
-    <title>Index</title>
+    <title>Tienda Tecnologica</title>
 </head>
 
 <body>
--- TiendaComputacion/View/soporte.php
+++ TiendaComputacion/View/soporte.php
@@ -8,7 +8,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <title>Productos</title>
+    <title>Soporte</title>
 </head>
 
 <body>
